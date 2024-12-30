c69f487c5f12e89f010eb286fc8694231f3c5b5b
davidgsieiro <151624547+davidgsieiro@users.noreply.github.com>
2024-12-30 17:53:40 +0100

    commit inicial

---
+++ javaApp_GIT/src/main/java/dgs/javaapp_git/JavaApp_GIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dgs.javaapp_git;
+
+/**
+ *
+ * @author David
+ */
+public class JavaApp_GIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
