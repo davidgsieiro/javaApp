3a28d15bb311b91c1bc38ce397618cdc3a618705
davidgsieiro <151624547+davidgsieiro@users.noreply.github.com>
2024-12-30 18:04:53 +0100

    Clases country y countrylanguage

---
+++ javaApp_GIT/src/main/java/Clases/Country.java
@@ -0,0 +1,162 @@
+package Clases;
+
+public class Country {
+    private String code;
+    private String name;
+    private Continente continent;
+    private String region;
+    private long surfaceArea;
+    private int indepYear;
+    private long population;
+    private double lifeExpectancy;
+    private double gnp;
+    private double gnpOld;
+    private String localName;
+    private String gobernmentForm;
+    private String headOfState;
+    private int capital;
+    private String code2;
+
+    public Country() {
+    }
+
+    public Country(String code, String name, Continente continent, String region, long surfaceArea, int indepYear, long population, double lifeExpectancy, double gnp, double gnpOld, String localName, String gobernmentForm, String headOfState, int capital, String code2) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.surfaceArea = surfaceArea;
+        this.indepYear = indepYear;
+        this.population = population;
+        this.lifeExpectancy = lifeExpectancy;
+        this.gnp = gnp;
+        this.gnpOld = gnpOld;
+        this.localName = localName;
+        this.gobernmentForm = gobernmentForm;
+        this.headOfState = headOfState;
+        this.capital = capital;
+        this.code2 = code2;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Continente getContinent() {
+        return continent;
+    }
+
+    public void setContinent(Continente continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public long getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public void setSurfaceArea(long surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public int getIndepYear() {
+        return indepYear;
+    }
+
+    public void setIndepYear(int indepYear) {
+        this.indepYear = indepYear;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    public double getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    public void setLifeExpectancy(double lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+    public double getGnp() {
+        return gnp;
+    }
+
+    public void setGnp(double gnp) {
+        this.gnp = gnp;
+    }
+
+    public double getGnpOld() {
+        return gnpOld;
+    }
+
+    public void setGnpOld(double gnpOld) {
+        this.gnpOld = gnpOld;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public String getGobernmentForm() {
+        return gobernmentForm;
+    }
+
+    public void setGobernmentForm(String gobernmentForm) {
+        this.gobernmentForm = gobernmentForm;
+    }
+
+    public String getHeadOfState() {
+        return headOfState;
+    }
+
+    public void setHeadOfState(String headOfState) {
+        this.headOfState = headOfState;
+    }
+
+    public int getCapital() {
+        return capital;
+    }
+
+    public void setCapital(int capital) {
+        this.capital = capital;
+    }
+
+    public String getCode2() {
+        return code2;
+    }
+
+    public void setCode2(String code2) {
+        this.code2 = code2;
+    }
+    
+    
+}
