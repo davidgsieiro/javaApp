58701646646650dba081e5d1a1a45bc8022f00c4
davidgsieiro <151624547+davidgsieiro@users.noreply.github.com>
2024-12-30 18:05:20 +0100

    enum continente

---
+++ javaApp_GIT/src/main/java/Clases/Continente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author David
+ */
+public enum Continente {
+    NORTH_AMERICA,
+    SOUTH_AMERICA,
+    ASIA,
+    AFRICA,
+    EUROPE,
+    OCEANIA,
+    ANTARCTICA
+    
+}
