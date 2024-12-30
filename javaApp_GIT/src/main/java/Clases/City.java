c88569185706ed08107a87556720e025175b7ac8
davidgsieiro <151624547+davidgsieiro@users.noreply.github.com>
2024-12-30 17:55:20 +0100

    Create City.java

---
+++ javaApp_GIT/src/main/java/Clases/City.java
@@ -0,0 +1,11 @@
+package Clases;
+
+public class City {
+    private int id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private long population;
+    
+    
+}
